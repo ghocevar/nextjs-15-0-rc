749958b307974615815febf9fea0393bd3670538
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2024-05-28 22:06:08 +0200

    Update React types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Counter from '@/components/Counter';
+import { unstable_noStore } from 'next/cache';
+import { cookies } from 'next/headers';
 import * as v from 'valibot';
 
 // export const dynamic = 'force-dynamic';
@@ -8,6 +10,8 @@ const GetTimeSchema = v.object({
 });
 
 const getTime = async () => {
+  // unstable_noStore();
+  console.log('cookies', cookies().getAll());
   const res = await fetch(
     'http://worldtimeapi.org/api/timezone/Europe/Ljubljana'
   );
